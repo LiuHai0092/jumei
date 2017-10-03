4d42d673b628ed4c5730d3c9c3bbdbb4e19f9ac3
LiuHai0092 <294488725@qq.com>
2017-10-03 17:36:31 +0800

    聚美

--- js/nav_right.js
+++ js/nav_right.js
@@ -14,3 +14,37 @@ $(".nav_right>ul li").hover(function() {
 		"margin-left": "-150px"
 	}).fadeOut();
 });
+//搜索框的下拉效果
+$(function() {
+	var his_cookie = $.cookie("HIS_COOKIE");
+	if(his_cookie != null) {
+		var his = his_cookie.split(",");
+		var html = '';
+		for(var i = 0; i < his.length; i++) {
+			html += '<a href=""><span>' + his[i] + '</span></a>';
+			//$(".his_search").append('<a href=""><span>'+his[i]+'</span></a>');
+		}
+		//					alert(html);
+		$(".his_search").html(html);
+	}
+	$(".search_box input").focus(function() {
+		$(".his_search").show();
+	});
+	$(".search_box input").blur(function() {
+		$(".his_search").hide();
+	});
+	//点击搜索
+	$(".search_box button").click(function() {
+		var searchText = $(this).prev().val().trim();
+		//追加cookie值
+		if(his_cookie != null) {
+			his_cookie = searchText + "," + his_cookie;
+		} else {
+			his_cookie = searchText;
+		}
+		if(searchText.length > 1) {
+			//						alert(his_cookie);
+			$.cookie("HIS_COOKIE", his_cookie);
+		}
+	})
+});
